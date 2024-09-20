e9aaf8f29273c6d523c03f198aea2f1ba556e5c7
Amlan Sarmah <amlansarmah26@gmail.com>
2024-09-20 18:45:14 +0530

    webapp login and sec

--- app/dagapur/clientlayout.tsx
+++ app/dagapur/clientlayout.tsx
@@ -1,13 +1,45 @@
-// ClientLayout.tsx
 "use client";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Login from "./login";
 
 const ClientLayout = ({ children }: { children: React.ReactNode }) => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const checkLoginStatus = () => {
+      const loginExpiration = localStorage.getItem("loginExpiration");
+      if (
+        loginExpiration &&
+        new Date().getTime() < parseInt(loginExpiration, 10)
+      ) {
+        setIsLoggedIn(true);
+      } else {
+        setIsLoggedIn(false);
+      }
+      setIsLoading(false);
+    };
+
+    checkLoginStatus();
+  }, []);
+
+  const handleLoginSuccess = () => {
+    // Set expiration to one month from now
+    const expirationDate = new Date();
+    expirationDate.setMonth(expirationDate.getMonth() + 1);
+    localStorage.setItem(
+      "loginExpiration",
+      expirationDate.getTime().toString()
+    );
+    setIsLoggedIn(true);
+  };
+
+  if (isLoading) {
+    return <div></div>; // Or any loading indicator
+  }
 
   if (!isLoggedIn) {
-    return <Login onLoginSuccess={() => setIsLoggedIn(true)} />;
+    return <Login onLoginSuccess={handleLoginSuccess} />;
   }
 
   return <>{children}</>;
